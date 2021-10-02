7c9c68b9ab9bae5384a1aa5db540dc91b2cc400a
id1752633745 <1752633745@qq.com>
2021-10-02 23:55:41 +0800

    首次提交

--- src/test/java/com/nowcoder/community/LoggerTest.java
+++ src/test/java/com/nowcoder/community/LoggerTest.java
@@ -1,21 +1,12 @@
 package com.nowcoder.community;
 
 
-import com.nowcoder.community.dao.AlphaDao;
-import com.nowcoder.community.service.AlphaService;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.test.context.ContextConfiguration;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @SpringBootTest
 @ContextConfiguration(classes = CommunityApplication.class)
